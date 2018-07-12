772095c7f06899875c8c71526d6516a55c4a057b
karthikakrishnaveni <41140795+karthikakrishnaveni@users.noreply.github.com>
2018-07-12 16:26:42 +0530

    Create checkchar.py

---
+++ checkchar.py
@@ -0,0 +1,9 @@
+num=input()
+str1=['a','e','i','o','u']
+str2=['$','@','*','#']
+if num in str1:
+            print("Vowel")
+elif num in str2:
+	print("invalid")
+else:
+            print("Consonant")
